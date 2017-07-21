13af9587e5dfa968bee0117d41890ff19a5bf150
abdullah-shiwani <abdullahshiwani@gmail.com>
2017-07-21 12:02:38 +0500

    Update Birthday-Cake-Candles.js
    default sort function works in Unicode code point order so sort data using custom compare technique.

--- algos/Birthday-Cake-Candles.js
+++ algos/Birthday-Cake-Candles.js
@@ -1,14 +1,15 @@
 
 /////////////// ignore above this line ////////////////////
-
 function birthdayCakeCandles(n, ar) {
-  const sortedAr = ar.sort();
+  const sortedAr = ar.sort( (a,b) => a - b);
   let remCanCount = 1;
-  for(let i = n-1; i >= 0; i--){
-    if(sortedAr[i] === sortedAr[i-1])
-      remCanCount++;
-    else
-      break;
+  for(let i = n-1; i > 0; i--){
+
+    if(sortedAr[i] !== sortedAr[i-1])
+        break;
+
+    remCanCount++;
+
   }
   return remCanCount;
 }
@@ -16,8 +17,7 @@ function birthdayCakeCandles(n, ar) {
 function main() {
     var n = parseInt(readLine());
     ar = readLine().split(' ');
-    ar = ar.map(Number);
+    ar = ar.map(Number).sort();
     var result = birthdayCakeCandles(n, ar);
     process.stdout.write("" + result + "\n");
-
 }
